c22ceacd8d96458aa719c29739a4c0f15e17b31a
ElGoru <ggarcia_97@hotmail.com>
2026-06-10 19:37:09 -0400

    fix

--- apps/api/src/middleware/validator.ts
+++ apps/api/src/middleware/validator.ts
@@ -1,5 +1,6 @@
 import { factory } from '@factory'
 import { zValidator } from '@hono/zod-validator'
+import { err } from 'neverthrow'
 import type { ZodType } from 'zod'
 
 export const queryValidator = <T>(schema: ZodType<T>) =>
@@ -7,7 +8,7 @@ export const queryValidator = <T>(schema: ZodType<T>) =>
     zValidator('query', schema, (result, c) => {
       if (!result.success) {
         c.var.logger('error', 'invalid query')(result.error)
-        return c.json({ message: 'Failed to validate' }, 400)
+        return c.var.appResponse(err({ type: 'ValidationError' as const, message: 'Failed to validate', input: result.error }))
       }
       c.var.logger('info', 'valid query')(result.data)
     })
